6cff01afcbd94ae6ab029395615923eaa29eec5a
txt <181250132@smail.nju.edu.cn>
2020-07-24 18:07:39 +0800

    每道题平均分

---
+++ 数据处理/average.py
@@ -0,0 +1,23 @@
+import json
+f = open('score.json', encoding='utf-8')
+cases=[]
+count=[]
+score=[]
+users={}
+res = f.read()
+data = json.loads(res)
+for key in data:
+    if key["case_id"] in cases:
+        i=cases.index(key["case_id"])
+        score[i]+=key["score"]
+        count[i]+=1
+    else:
+        cases.append(key["case_id"])
+        count.append(1)
+        score.append(key["score"])
+for i in range(0,len(cases)):
+    score[i]=score[i]/count[i]
+    users[cases[i]]=score[i]
+filename = 'average.json'
+with open(filename, 'w', encoding='utf-8') as file_obj:
+    json.dump(users, file_obj, ensure_ascii=False, indent=4)
---
+++ 数据处理/score.py
@@ -0,0 +1,18 @@
+import json
+import os
+f = open('test_data.json', encoding='utf-8')
+res = f.read()
+data = json.loads(res)
+users=[]
+for key in data:
+    cases = data[key]['cases']
+    user_id = str(data[key]['user_id'])
+
+    for case in cases:
+        Mycase={"case_id":case["case_id"],
+                "score":case["final_score"]}
+        users.append(Mycase)
+
+filename = 'score.json'
+with open(filename, 'w', encoding='utf-8') as file_obj:
+    json.dump(users, file_obj, ensure_ascii=False, indent=4)
